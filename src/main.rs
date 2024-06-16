0d55148a62c36f001cbd16c790c0d3740b7b1099
y_mc <momo.toure27@gmail.com>
2024-06-16 17:54:42 +0200

    coding

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,29 @@ use std::io;
 
 
 fn main() {
+    println!("Quel est votre nom ?");
+    let mut nom = String::new();
+    io::stdin().read_line(&mut nom).expect("Failed to read line");
+    let mut age =  String::new();
+    println!("Quel est votre age ?");
+    io::stdin().read_line(&mut age).expect("Failed to read line");
+    let age: i32 = age.trim().parse().expect("Please type a number!");
+    println!("Quelle est votre taille ?");
+    let mut taille = String::new();
+    io::stdin().read_line(&mut taille).expect("Failed to read line");
+    let taille: i32 = taille.trim().parse().expect("Please type a number!");
 
-    let nom = "Momo";
-    let age = 25;
-    let taille = 1.75;
+
+
     println!("Hello, world!");
-    println!("Je m'appelle {} et j'ai {} ans et je mesure {}m", nom, age, taille);
+    println!("Nom: {}", nom);
+    println!("Age: {}", age);
+    println!("Taille: {}", taille);
+    println!();
     //println!("Random Number: {}",random())
 
 
-    for i in 1..10 {
+    for i in 1..3 {
         println!("{} random numb ; {}",i,random());
         thread::sleep(Duration::from_secs(1));
 
