a5d8571f560141032315df38545c22709a2051a6
y_mc <momo.toure27@gmail.com>
2024-07-14 15:00:58 +0200

    game

--- src/main.rs
+++ src/main.rs
@@ -4,110 +4,134 @@ use std::time::Duration;
 use std::io;
 
 
-// struct Player {
-//     nom: String,
-//     age:i32,
-//     taille:i32,
-//     score:i32,
-// }
 
+
+#[derive(Debug)]
+enum Niveau {
+    Debutant,
+    Intemediare,
+    Expert,
+}
+
+struct Player {
+    nom : String,
+    id : i32,
+    score : Vec<i32>,
+    xp : i32,
+    niveau: Niveau,
+}
 
 fn main() {
+    println!("Hallo World");
+    println!("Veuillez entre le  numbre  d'iteratation ");
 
-    println!("Player 1 ");
-    let player_1 = input();
-    println!("Player 2");
-    let player_2 = input();
-    let mut score_1 :i32 = 0;
-    let mut score_2 :i32 = 0;
-    println!("Nombre de game ");
-    let game:i32 = input_numb();
-    let mut runde= 0;
-
-    println!("PLayer 1: {}",player_1);
-    println!("Player 2: {}",player_2);
-    println!("Number of Runde : {}",runde);
-    println!("Bienvenue a {} et {}",player_1,player_2);
-    thread::sleep(Duration::from_secs(2));
-    println!("On Commence ......");
-    thread::sleep(Duration::from_secs(5));
+    let  numb_iteration = input_numb();
+    println!("Nom du Joueur ?  ");
 
 
-    while runde < game {
-        runde = runde+1;
-        println!("Rund :{}",runde);
-        let  dice_1 = random();
-        let dice_2 = random();
-        thread::sleep(Duration::from_secs(2));
-        println!("{} : Dice:{} ",player_1 , dice_1 );
-        // let dice_1 = random();
-        thread::sleep(Duration::from_secs(1));
-        println!("{} : Dice:{} ",player_2,dice_2);
-        if dice_1 > dice_2 {
-            thread::sleep(Duration::from_secs(1));
-            println!("{} Won this {} rund",player_1,runde);
-            score_1 = score_1+1;
-            thread::sleep(Duration::from_secs(2));
-
-        } else {
-            thread::sleep(Duration::from_secs(1));
-            println!("{} Won This {}",player_2,runde);
-            score_2 = score_2 + 1 ;
-            thread::sleep(Duration::from_secs(2));
+    let mut  joueur = Player{
+        nom : input(),
+        id : random(1),
+        score: Vec::new(),
+        xp : 0,
+        niveau : Niveau::Debutant
+    };
 
 
-        }
+    println!("Player 1 : {} ", joueur.nom);
+    println!("id:{}",joueur.id);
+    println!("Score : {:?}",joueur.score);
 
-
-
-
-
+    for i in 0..numb_iteration{
+        let dee = random(2);
+        println!("{}Nombre genere : {}",i, dee);
+        joueur.score.push(dee);
     }
 
+    println!("Score : {:?}",joueur.score);
+    joueur.xp = joueur.score.iter().sum();
+    println!("XP : {}",joueur.xp);
 
-    if score_1 > score_2 {
-        thread::sleep(Duration::from_secs(2));
 
-        println!("{} Won the Match",player_1);
 
-    } else {
-        thread::sleep(Duration::from_secs(2));
+    if joueur.xp > 100 {
+        joueur.niveau = Niveau::Intemediare;
 
-        println!("{} Won the match",player_2);
+    }else {
+        joueur.niveau = Niveau::Debutant;
+
     }
+    pause();
+
+    println!("Niveau : {:?}",joueur.niveau);
 
 
 
 
 
-    //
-    // println!("Combien de nombre aléatoire voulez-vous ?");
-    // let mut numb_inter = String::new();
-    // io::stdin().read_line(&mut numb_inter).expect("Failed to read line");
-    // let numb_inter: i32 = numb_inter.trim().parse().expect("Please type a number!");
-    //
-    //
 
-
-    // println!("Hello, world!");
-    // println!("Nom: {}", player1.nom);
-    // println!("Age: {}", player1.age);
-    // println!("Taille: {}", player1.taille);
-    // println!("Score:{}",player1.score);
-
-    println!("#######FIN DU JEUX#########");
-    //println!("Random Number: {}",random())
-
-
-    // for i in 1..runde {
-    //     println!("{} random numb ; {}",i,random());
-    //     thread::sleep(Duration::from_secs(1));
-    //
-    // }
-
-
+   //  println!("Player 2");
+   //  let player_2 = input();
+   //  let mut score_1 :i32 = 0;
+   //  let mut score_2 :i32 = 0;
+   //  println!("Nombre de game ");
+   //  let game:i32 = input_numb();
+   //  let mut runde= 0;
+   //
+   //  println!("PLayer 1: {}",player_1);
+   //  println!("Player 2: {}",player_2);
+   //  println!("Number of Runde : {}",runde);
+   //  println!("Bienvenue a {} et {}",player_1,player_2);
+   //  thread::sleep(Duration::from_secs(2));
+   //  println!("On Commence ......");
+   //  thread::sleep(Duration::from_secs(5));
+   //
+   //
+   //  while runde < game {
+   //      runde = runde+1;
+   //      println!("Rund :{}",runde);
+   //      let  dice_1 = random();
+   //      let dice_2 = random();
+   //      thread::sleep(Duration::from_secs(2));
+   //      println!("{} : Dice:{} ",player_1 , dice_1 );
+   //      // let dice_1 = random();
+   //      thread::sleep(Duration::from_secs(1));
+   //      println!("{} : Dice:{} ",player_2,dice_2);
+   //      if dice_1 > dice_2 {
+   //          thread::sleep(Duration::from_secs(1));
+   //          println!("{} Won this {} rund",player_1,runde);
+   //          score_1 = score_1+1;
+   //          thread::sleep(Duration::from_secs(2));
+   //
+   //      } else {
+   //          thread::sleep(Duration::from_secs(1));
+   //          println!("{} Won This {}",player_2,runde);
+   //          score_2 = score_2 + 1 ;
+   //          thread::sleep(Duration::from_secs(2));
+   //
+   //
+   //      }
+   //
+   //  }
+   //
+   //  if score_1 > score_2 {
+   //      thread::sleep(Duration::from_secs(2));
+   //      println!("{} Won the Match",player_1);
+   //
+   //  } else {
+   //      thread::sleep(Duration::from_secs(2));
+   //      println!("{} Won the match",player_2);
+   //  }
+   //
+   //
+   //  println!("#######FIN DU JEUX#########");
+   //
 
 }
+
+
+
+
 
 fn input() -> String {
     // println!("enregistrer votre nom");
@@ -124,9 +148,23 @@ fn input_numb() -> i32{
 
 }
 
-fn random() -> i32 {
-    let mut gen = thread_rng();
-    let random_numb = gen.gen_range(0..6);
-    return random_numb ;
+fn random(factor:i32) -> i32 {
+    if factor == 1 {
+        let mut gen = thread_rng();
+        let random = gen.gen_range(0..10);
+        return random;
+    }else if factor == 2 {
+        let mut gen = thread_rng();
+        let random = gen.gen_range(0..100);
+        return random;
+    }
+    else {
+        let mut gen = thread_rng();
+        let random = gen.gen_range(0..1000);
+        return random;
+    }
+}
 
+fn pause() {
+    thread::sleep(Duration::from_secs(1));
 }
